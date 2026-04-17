a8e0887c9001b5ea13da64223f5e255573b03467
aliiexe <alibusinessbourak@gmail.com>
2026-04-17 11:12:31 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+// Define a route (endpoint)
+app.get('/', (req, res) => {
+res.send('Hello DevSecOps 🚀');
+});
+// Start server on port 8080
+app.listen(8080, () => console.log('Running on port 8080'));
